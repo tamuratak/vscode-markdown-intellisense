3ea63f486184198fc89fad707dd2f77c9fb457c0
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-07-03 08:07:51 +0900

    add src/

---
+++ src/embedContentProvider.ts
@@ -0,0 +1,11 @@
+import {TextDocumentContentProvider, Uri} from 'vscode'
+
+export class EmbedContentProvider implements TextDocumentContentProvider {
+
+    constructor() {}
+
+    provideTextDocumentContent(uri: Uri) {
+        
+    }
+
+}
