e31882d4c320af074121014eb4b7547b677b23b3
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-07-06 09:00:07 +0900

    add findNode

--- src/astUtil.ts
+++ src/astUtil.ts
@@ -3,9 +3,11 @@ import remark = require('remark');
 import math = require('remark-math');
 const processor = remark().use(math);
 
-type Node = {
+export type Node = {
     type: string;
     children?: Node[];
+    lang?: string;
+    meta?: string;
     value?: string;
     position: {
         start: { line: number, column: number, offset: number };
@@ -14,6 +16,34 @@ type Node = {
     }
 }
 
+function isNodeIncludingPosition(node: Node, position: vscode.Position) {
+    const {start, end} = node.position
+    const range = new vscode.Range(start.line - 1, start.column - 1, end.line - 1, end.column - 1)
+    return range.contains(position)
+}
+
 export function findNode(text: string, position: vscode.Position) {
     const ast: Node = processor.parse(text)
+    const nodeArrayStack: Node[][] = []
+    if (ast.children) {
+        nodeArrayStack.push(ast.children)
+    }
+    while (true) {
+        const children = nodeArrayStack.pop()
+        if (children === undefined) {
+            break
+        }
+        for (const node of children) {
+            if (node.type.match(/text|code|html|inlineMath|math/)) {
+                if (isNodeIncludingPosition(node, position)) {
+                    return node
+                }
+            } else {
+                if (node.children) {
+                    nodeArrayStack.push(node.children)
+                }
+            }
+        }
+    }
+    return undefined
 }
