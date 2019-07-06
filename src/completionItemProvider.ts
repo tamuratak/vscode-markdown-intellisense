33e2ca57e58895c59350e5631794c1e65f581fc5
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-07-06 22:53:55 +0900

    add src/completionItemProvider.ts

---
+++ src/completionItemProvider.ts
@@ -0,0 +1,38 @@
+import {CompletionContext, CompletionList, CompletionItemProvider, Position, TextDocument, commands} from 'vscode'
+import {findNode} from './astUtil'
+import {createVirtualDocument, createVirtualPosition, openVirtualDocument} from './virtualDocument'
+
+
+export class MarkdownCompletionItem implements CompletionItemProvider {
+
+    constructor() {}
+
+    async provideCompletionItems(document: TextDocument, position: Position, _: any, context: CompletionContext) {
+        const node = findNode(document.getText(), position)
+        if (!node) {
+            return
+        }
+        const virtualDocUri = createVirtualDocument(document, node)
+        const virtualPosition = createVirtualPosition(position, node)
+        const virtualDoc = await openVirtualDocument(virtualDocUri, node)
+        if (!virtualDoc || !virtualPosition) {
+            return
+        }
+        const itemList = await commands.executeCommand<CompletionList>(
+            'vscode.executeCompletionItemProvider',
+            virtualDocUri,
+            virtualPosition,
+            context.triggerCharacter,
+            10
+        )
+        if (!itemList){
+            return
+        }
+        for (const item of itemList.items) {
+            item.range = undefined
+            item.textEdit = undefined
+        }
+        return itemList
+    }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import * as vscode from 'vscode'
 import {MarkdownHoverProvider} from './hoverProvider'
+import {MarkdownCompletionItem} from './completionItemProvider'
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('IntelliSense.md activated')
     context.subscriptions.push(
         vscode.languages.registerHoverProvider(
-            { scheme: 'file', language: 'markdown'},
+            { scheme: 'file', language: 'markdown' },
             new MarkdownHoverProvider()
+        )
+    )
+
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            { scheme: 'file', language: 'markdown' },
+            new MarkdownCompletionItem()
         )
     )
 }
