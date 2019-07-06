0e43eb04e8f1cdb137e1d95e8af60f3c1b3166be
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-07-06 11:29:06 +0900

    follow tslint

--- test/vscode/suite/extension.test.ts
+++ test/vscode/suite/extension.test.ts
@@ -9,18 +9,18 @@ import {findNode} from '../../../src/astUtil'
 
 const workspaceRoot = process.env.CODE_WORKSPACE_ROOT || process.env.PWD || ''
 if (workspaceRoot === '') {
-    assert.fail("workspaceRoot path must be set.")
+    assert.fail('workspaceRoot path must be set.')
 }
 
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("test IntelliSense.md", function () {
+suite('test IntelliSense.md', function () {
 
-    suiteTeardown("", async () => {
+    suiteTeardown('', async () => {
 
     })
 
-    test("test findNode", async function() {
+    test('test findNode', async function() {
         const text = `abc`
         const pos = new vscode.Position(0, 2)
         const ret = findNode(text, pos)
